ce94a196ea085d11011ef2653f3d51271caf25a9
samdan <samdan@ijikod.com>
2020-11-19 08:00:57 +0000

    Creating the application component

---
+++ app/src/main/java/com/ijikod/di/application/ApplicationComponent.kt
@@ -0,0 +1,17 @@
+package com.ijikod.di.application
+
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component
+interface ApplicationComponent {
+
+    @Component.Factory
+    interface Factory {
+
+        fun create(@BindsInstance context: Context): ApplicationComponent
+    }
+}
