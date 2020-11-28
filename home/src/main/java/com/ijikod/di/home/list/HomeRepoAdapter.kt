264c0f285660e5f2e20449185324f6c291e5904f
samdan <samdan@ijikod.com>
2020-11-28 07:07:51 +0000

    implemented recycler view adapter set up

---
+++ home/src/main/java/com/ijikod/di/home/list/HomeRepoAdapter.kt
@@ -0,0 +1,45 @@
+package com.ijikod.di.home.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ijikod.di.home.databinding.RepoItemBinding
+
+class HomeRepoAdapter : RecyclerView.Adapter<HomeRepoAdapter.ReposItemViewHolder>() {
+
+
+    inner class ReposItemViewHolder(private val binding: RepoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
+        fun bind(repoItem: RepoItem) {
+            binding.appName.text = repoItem.name
+            binding.appDecription.text = repoItem.description
+            binding.forkCount.text = repoItem.forkCount.toString()
+            binding.starCount.text = repoItem.startCount.toString()
+        }
+    }
+
+
+    private val data: MutableList<RepoItem> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReposItemViewHolder {
+        val binding = RepoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ReposItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ReposItemViewHolder, position: Int) {
+        with(holder) {
+            bind(data[position])
+        }
+    }
+
+    override fun getItemCount(): Int = data.size
+
+
+    fun setRepoItems(repoItems: List<RepoItem>) {
+        data.clear()
+        data.addAll(repoItems)
+        notifyDataSetChanged()
+    }
+}
---
+++ home/src/main/java/com/ijikod/di/home/list/RepoItem.kt
@@ -0,0 +1,8 @@
+package com.ijikod.di.home.list
+
+data class RepoItem(
+    val name: String,
+    val description: String,
+    val startCount: Int,
+    val forkCount: Int
+)
