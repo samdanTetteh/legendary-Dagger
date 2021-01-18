5d4841c70f09ec6ed529b4ede03e9a64044a7c9e
samdan <samdan@ijikod.com>
2021-01-18 06:17:29 +0000

    implemented basic caching in repository and setup for contributors

--- repository/src/main/java/com/ijikod/di/repository/AppRepository.kt
+++ repository/src/main/java/com/ijikod/di/repository/AppRepository.kt
@@ -1,6 +1,7 @@
 package com.ijikod.di.repository
 
 import com.ijikod.di.githubapi.GitHubApi
+import com.ijikod.di.githubapi.model.ContributorApiModel
 import com.ijikod.di.githubapi.model.RepoApiModel
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,8 +11,36 @@ class AppRepository @Inject constructor(
     private val gitHubApi: GitHubApi
 ) {
 
+    private val cachedRepos = mutableListOf<RepoApiModel>()
+
     suspend fun getTopGitHubRepos(): List<RepoApiModel> {
-        return gitHubApi.getTopRepositories().items
+        if (cachedRepos.isEmpty()){
+            cachedRepos.addAll(gitHubApi.getTopRepositories().items)
+        }
+        return cachedRepos
     }
+
+    suspend fun getRepo(repoOwner: String, repoName: String): RepoApiModel {
+        return getRepoFromCache(repoOwner, repoName) ?: gitHubApi.getRepo(repoOwner, repoOwner)
+    }
+
+    suspend fun getContributors(repoOwner: String, repoName: String): List<ContributorApiModel> {
+        return gitHubApi.getContributors(repoOwner, repoName)
+    }
+
+
+    private fun getRepoFromCache(repoOwner: String, repoName: String): RepoApiModel?{
+        return cachedRepos.firstOrNull {repo ->
+            repo.owner.login == repoOwner && repo.name == repoName
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
