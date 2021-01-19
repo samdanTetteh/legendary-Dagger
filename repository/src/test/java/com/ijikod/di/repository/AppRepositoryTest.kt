d1320fa770997aed08e2588e4ded0d747edac1cd
samdan <samdan@ijikod.com>
2021-01-19 06:36:03 +0000

    updated app repository unit tests

--- repository/src/test/java/com/ijikod/di/repository/AppRepositoryTest.kt
+++ repository/src/test/java/com/ijikod/di/repository/AppRepositoryTest.kt
@@ -3,6 +3,7 @@ package com.ijikod.di.repository
 import com.google.common.truth.Truth.assertThat
 import com.ijikod.app.githubapi.FakeGitHubApi
 import com.ijikod.di.githubapi.GitHubApi
+import com.ijikod.di.githubapi.model.ContributorApiModel
 import com.ijikod.di.githubapi.model.RepoApiModel
 import com.ijikod.di.githubapi.model.UserApiModel
 import kotlinx.coroutines.runBlocking
@@ -17,7 +18,6 @@ val FAKE_REPO =
         owner = UserApiModel(id = 1L, login = "Dagger"),
         stargazersCount = 1,
         forkCount = 1,
-        starCount = 1,
         contributorsUrl = "",
         createdDate = "1/01/2021",
         updatedDate = "1/02/2021"
@@ -37,10 +37,84 @@ class AppRepositoryTest {
     }
 
     @Test
-    fun `check_successful_query`() {
+    fun `getTopRepos returns result from GITHubApi`() {
         val topReps = runBlocking { appRepository.getTopGitHubRepos() }
 
         assertThat(topReps.size).isEqualTo(2)
         assertThat(topReps[0]).isEqualTo(FAKE_REPO)
     }
+
+    @Test
+    fun `getTopRepos returns cached result`(){
+        val initialRequest = runBlocking { appRepository.getTopGitHubRepos() }
+
+        //Change API return value
+        fakeGitHubApi.repos = listOf(FAKE_REPO)
+
+        val secondRequest = runBlocking { appRepository.getTopGitHubRepos() }
+
+        assertThat(initialRequest).isEqualTo(secondRequest)
+    }
+
+    @Test
+    fun `getRepo returns cached value`(){
+        //seed cache
+        runBlocking {
+            appRepository.getTopGitHubRepos()
+        }
+
+        //Set Api to return different model on single repo fetch
+        fakeGitHubApi.singleRepoResult = FAKE_REPO.copy(name = "Tobi Josh")
+
+        val singleRepoFetchResult = runBlocking {
+            appRepository.getRepo(
+                    repoOwner = FAKE_REPO.owner.login,
+                    repoName = FAKE_REPO.name
+            )
+        }
+
+        assertThat(singleRepoFetchResult).isEqualTo(FAKE_REPO)
+    }
+
+    @Test
+    fun `getRepo returns API value if not in cache`(){
+        //seed cache
+        runBlocking {
+            appRepository.getTopGitHubRepos()
+        }
+
+        val expectedModel = FAKE_REPO.copy(name = "Nilandri")
+        fakeGitHubApi.singleRepoResult = expectedModel
+
+        val singleRepoFetchResult = runBlocking {
+            appRepository.getRepo(
+                    repoName = expectedModel.name,
+                    repoOwner = expectedModel.owner.login
+            )
+        }
+
+        assertThat(singleRepoFetchResult).isEqualTo(expectedModel)
+    }
+
+    @Test
+    fun `getContributors returns API value`(){
+        val expectedContributors = listOf(
+                ContributorApiModel(
+                        id = 1L,
+                        login = "contributor",
+                        avatarUrl = "avatar.png"
+                )
+        )
+
+        fakeGitHubApi.contributorsResults = expectedContributors
+
+        val contributors = runBlocking {
+            appRepository.getContributors(FAKE_REPO.owner.login, FAKE_REPO.name)
+        }
+
+        assertThat(contributors).isEqualTo(expectedContributors)
+    }
+
+
+
 }
--- testing/app/src/main/java/com/ijikod/app/githubapi/FakeGitHubApi.kt
+++ testing/app/src/main/java/com/ijikod/app/githubapi/FakeGitHubApi.kt
@@ -2,7 +2,9 @@ package com.ijikod.app.githubapi
 
 import com.ijikod.di.githubapi.GitHubApi
 import com.ijikod.di.githubapi.TopRepoSearchResult
+import com.ijikod.di.githubapi.model.ContributorApiModel
 import com.ijikod.di.githubapi.model.RepoApiModel
+import java.lang.NullPointerException
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,10 +12,20 @@ import javax.inject.Singleton
 class FakeGitHubApi @Inject constructor() : GitHubApi {
 
     var repos = listOf<RepoApiModel>()
+    var singleRepoResult: RepoApiModel? = null
+    var contributorsResults = listOf<ContributorApiModel>()
 
 
     override suspend fun getTopRepositories(): TopRepoSearchResult {
         return TopRepoSearchResult(repos)
+    }
+
+    override suspend fun getRepo(repoOwner: String, repoName: String): RepoApiModel {
+        return singleRepoResult ?: throw NullPointerException("singleRepoResult was not set")
+    }
+
+    override suspend fun getContributors(repoOwner: String, repoName: String): List<ContributorApiModel> {
+        return contributorsResults
     }
 
 }
