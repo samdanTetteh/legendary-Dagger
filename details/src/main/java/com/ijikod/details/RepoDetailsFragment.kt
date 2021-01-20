357814771960005485b11a3fbf2b285d6133150c
samdan <samdan@ijikod.com>
2021-01-20 08:35:58 +0000

    implemented hooking up fragments with view states from view model

--- details/src/main/java/com/ijikod/details/RepoDetailsFragment.kt
+++ details/src/main/java/com/ijikod/details/RepoDetailsFragment.kt
@@ -1,17 +1,22 @@
 package com.ijikod.details
 
 import android.content.Context
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ijikod.details.databinding.ScreenDetailsBinding
+import com.ijikod.details.list.ContributorsAdapter
 import com.ijikod.di.di.viewmodel.AppViewModelFactory
 import javax.inject.Inject
 
-class RepoDetailsFragment: Fragment() {
+class RepoDetailsFragment : Fragment() {
 
     companion object {
         fun newInstance(repoOwner: String, repoName: String): RepoDetailsFragment {
@@ -24,7 +29,8 @@ class RepoDetailsFragment: Fragment() {
         }
     }
 
-    @Inject lateinit var viewModelFactory: AppViewModelFactory
+    @Inject
+    lateinit var viewModelFactory: AppViewModelFactory
 
     private val viewModel by lazy {
         ViewModelProvider(this, viewModelFactory)[RepoDetailsViewModel::class.java]
@@ -43,6 +49,72 @@ class RepoDetailsFragment: Fragment() {
     ): View? {
         val binding = ScreenDetailsBinding.inflate(inflater, container, false)
 
+        setUpContributorList(binding)
+
+        observeViewModeStates(binding)
         return binding.root
+    }
+
+    private fun observeViewModeStates(binding: ScreenDetailsBinding) {
+        viewModel.repoInfoUpdates.observe(viewLifecycleOwner, Observer { state ->
+            when (state) {
+                is RepoInfoViewStateLoading -> handleInfoLoadingState(binding)
+                is RepoInfoViewStateLoaded -> handleInfoLoadedState(binding, state)
+            }
+        })
+
+        viewModel.contributorsUpdates.observe (viewLifecycleOwner, Observer { state ->
+            when (state) {
+                is RepoContributorsViewStateLoading -> handleContributorsLoadingList(binding)
+                is RepoContributorsViewStateLoaded -> handleContributorsLoadedList(binding, state)
+            }
+        })
+    }
+
+    private fun handleContributorsLoadedList(binding: ScreenDetailsBinding, state: RepoContributorsViewStateLoaded) {
+        binding.contributorsLoadingIndicator.visibility = View.GONE
+        binding.contributorList.visibility = View.VISIBLE
+
+        (binding.contributorList.adapter as ContributorsAdapter).setContributors(state.contributors)
+    }
+
+    private fun handleContributorsLoadingList(binding: ScreenDetailsBinding) {
+        binding.detailsLoadingIndicator.visibility = View.VISIBLE
+        binding.contributorList.visibility = View.GONE
+    }
+
+    private fun handleInfoLoadedState(binding: ScreenDetailsBinding, state: RepoInfoViewStateLoaded) {
+        binding.detailsLoadingIndicator.visibility = View.GONE
+        binding.toggleInfoTextVisibility(View.VISIBLE)
+
+        binding.repoName.text = state.repoName
+        binding.repoDescription.text = state.repoDescription
+        binding.creationDate.text = state.createdDate
+        binding.updatedDate.text = state.updatedDate
+    }
+
+    private fun handleInfoLoadingState(binding: ScreenDetailsBinding) {
+        binding.detailsLoadingIndicator.visibility = View.VISIBLE
+        binding.toggleInfoTextVisibility(View.GONE)
+    }
+
+
+    private fun setUpContributorList(binding: ScreenDetailsBinding) {
+        binding.contributorList.apply {
+            adapter = ContributorsAdapter()
+            layoutManager = LinearLayoutManager(context)
+            addItemDecoration(
+                DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+            )
+        }
+    }
+
+    private fun ScreenDetailsBinding.toggleInfoTextVisibility(visibility: Int) {
+        repoName.visibility = visibility
+        repoDescription.visibility = visibility
+        createdDateLabel.visibility = visibility
+        creationDate.visibility = visibility
+        updatedDateLabel.visibility = visibility
+        updatedDate.visibility = visibility
     }
 }
