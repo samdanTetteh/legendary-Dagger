e42133f391235139cb80bd48b27b2be2f5bc3755
samdan <samdan@ijikod.com>
2021-01-21 06:05:46 +0000

    implemented custom nagivation

---
+++ navigation/src/main/java/com/ijikod/navigation/ScreenNavigator.kt
@@ -0,0 +1,31 @@
+package com.ijikod.navigation
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import java.lang.NullPointerException
+
+sealed class Screen
+data class DetailsScreen (val repoOwner: String, val repoName: String) : Screen()
+
+interface ScreenNavigator {
+    fun goToScreen(screen: Screen)
+}
+
+
+interface NavigationDeps {
+    fun screenNavigation() : ScreenNavigator
+}
+
+const val NAVIGATION_DEPS_SERVICE = "com.sample.githubbrowser.navigationdeps"
+
+fun Activity.navigationDeps(): NavigationDeps {
+
+    @SuppressLint("WrongConstant")
+    val navigationDeps = getSystemService(NAVIGATION_DEPS_SERVICE) as? NavigationDeps
+        ?: applicationContext.getSystemService(NAVIGATION_DEPS_SERVICE) as? NavigationDeps
+
+    return navigationDeps ?: throw NullPointerException (
+        "Activity must override getSystemService and provide NagivationDeps " +
+                " for service name: $NAVIGATION_DEPS_SERVICE"
+    )
+}
