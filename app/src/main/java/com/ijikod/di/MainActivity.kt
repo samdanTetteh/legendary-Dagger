e27b3859c5ebc0aa39fa184fe3991eaa49fff90e
samdan <samdan@ijikod.com>
2021-01-22 05:50:44 +0000

    implemented activity scoped component

--- app/src/main/java/com/ijikod/di/MainActivity.kt
+++ app/src/main/java/com/ijikod/di/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ijikod.di
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ijikod.di.application.MainActivityComponent
+import com.ijikod.di.application.injectAndGetComponent
 import com.ijikod.di.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var component: MainActivityComponent
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        component = injectAndGetComponent()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
